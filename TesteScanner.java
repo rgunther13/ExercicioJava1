b14423999b653eb51709cc629851cb4cde83a520
Massa_Joe <60799887+rgunther13@users.noreply.github.com>
2020-02-21 21:24:40 -0300

    ScannerJava
    Scanner

---
+++ TesteScanner.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+class TesteScanner{
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        
+        System.out.println("Digite um inteiro: ");
+        int a = input.nextInt();
+        System.out.println("Valor a " + a);
+        
+        System.out.println("Digite um double: ");
+        double b = input.nextDouble();
+        System.out.printf("%f \n", b);
+
+        input.nextLine();
+        System.out.println("Digite um nome: ");
+        String nome = input.nextLine();
+        System.out.print(nome);
+    }
+    
+}
---
+++ scanner.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+class TesteScanner{
+
+    public static void main(String[] args){
+        Scanner input
+  } 
+
+}
