9d64e5455f6070dec02d41d0636cf58acaca0db3
Massa_Joe <60799887+rgunther13@users.noreply.github.com>
2020-02-28 19:34:45 -0300

    Exercicio Correto

--- mediaehmaior.java
+++ mediaehmaior.java
@@ -1,39 +1,60 @@
 import java.util.Scanner;
 
 class mediaehmaior{
-    public static void main(String[] args){
-        Scanner input = new Scanner(System.in);
+    public static void main(final String[] args) {
+        final Scanner input = new Scanner(System.in);
 
         System.out.println("Digite um numero de ponto flutuante: ");
-        float a = input.nextFloat();
+        final float a = input.nextFloat();
         input.nextLine();
-                
+
         System.out.println("Digite outro numero de ponto flutuante: ");
-        float b = input.nextFloat();
+        final float b = input.nextFloat();
         input.nextLine();
 
         System.out.println("Digite um numero de ponto flutuante: ");
-        float c = input.nextFloat();
+        final float c = input.nextFloat();
         input.nextLine();
      
                 
         System.out.println("A soma dos dois numeros eh: " + (a+b+c));
-        System.out.println("A media dos 3 numeros eh: " + ((a+b+c)/3);
+        System.out.println("A media dos 3 numeros eh: " + ((a+b+c)/3));
         System.out.println("A multiplicacao dos 3 numeros eh: " + (a*b*c));
-        int maior;
-        if (a > b && a >c)
+        float maior;
+        float menor;
+        if (a > b && a >c){
             maior = a;
-        else if (b > a && b > c)
-            maior = b;
-        else 
-            maior = c;
+            if (b < c){
+                menor = b;
+            }
+            else{
+                menor = c;
+            }
 
+        }
+        else if (b > a && b > c){
+            maior = b;
+            if (a < c){
+                menor = a;
+            }
+            else{
+                menor = c;
+            }
             
-            System.out.println("Os numeros sao IGUAIS!!!");
-        else if (a > b)
-            System.out.println("O numero" + a + " eh MAIOR!!");
-        else 
-            System.out.println("O numero" + b + " eh MAIOR!!");
+        }
+        else {
+            maior = c;
+            if (a < b){
+                menor = a;
+            }
+            else{
+                menor = b;
+            }
+
+        }    
+        System.out.println("O numero " + maior + " eh MAIOR!!");
+        System.out.println("O numero " + menor + " eh MENOR!!");
+      
 
         
     }
